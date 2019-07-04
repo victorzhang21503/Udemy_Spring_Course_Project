479eabe71141418ba30b27879a7ecbfc997c5abd
Mengyuan <mzhan123@asu.edu>
2019-07-04 14:01:12 -0700

    IoC, Dependency Injection by XML Configuration

---
+++ src/com/luv2code/springdemo/MyConstructor.java
@@ -0,0 +1,16 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MyConstructor {
+
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		Coach theCoach = context.getBean("myTrackCoach"
+				+ "", Coach.class);
+		theCoach.getDailyWorkout();
+		System.out.println(theCoach.getDailyFortune());
+		context.close();
+	}
+
+}
